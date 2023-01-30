0055f37de30069dccf1ebb0d0829def2d5a22770
kshitija-wakchaure <wakchaurekshitija24@gmail.com>
2023-01-30 14:09:08 +0530

    Country Task using forEach loop

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+var cl = console.log;
+
+var imgContainer = document.getElementById("imgContainer");
+var result = "";
+countries.forEach(function(flagg){
+    result += `<div class="col-lg-3 col-md-6 col-xs-12">
+                    <div class="card">
+                        <figure class="countryCard">
+                            <img src="${flagg.flag}" alt="Flag Here">
+                            <figcaption>
+                                <div class="countryName">
+                                    <h2>${flagg.name}</h2>
+                                </div>
+                                <div class="countryData">
+                                    <ul>
+                                        <li><strong>Capital: </strong>${flagg.capital}</li>
+                                        <li><strong>Language: </strong>${flagg.languages} </li>
+                                        <li><strong>Population: </strong>${flagg.population}</li>
+                                    </ul>
+                                </div>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>`;
+})
+imgContainer.innerHTML = result;
